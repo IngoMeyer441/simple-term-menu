d1db9974d36af6d008f510ee87416a0a71cbc62b
Ingo Meyer <i.meyer@fz-juelich.de>
2020-11-06 15:27:26 +0100

    Increase the version number to v0.10.4

--- simple_term_menu.py
+++ simple_term_menu.py
@@ -40,7 +40,7 @@ __author__ = "Ingo Meyer"
 __email__ = "i.meyer@fz-juelich.de"
 __copyright__ = "Copyright © 2019 Forschungszentrum Jülich GmbH. All rights reserved."
 __license__ = "MIT"
-__version_info__ = (0, 10, 3)
+__version_info__ = (0, 10, 4)
 __version__ = ".".join(map(str, __version_info__))
 
 
